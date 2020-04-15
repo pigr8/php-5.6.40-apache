4b188d7b919fc9529baf16da56af12c68915fc79
pigr8 <16479888+pigr8@users.noreply.github.com>
2020-04-15 21:25:13 +0200

    Update init_container.sh

--- init_container.sh
+++ init_container.sh
@@ -14,11 +14,6 @@ PHP version : `php -v | head -n 1 | cut -d ' ' -f 2`
 EOL
 cat /etc/motd
 
-# Get environment variables to show up in SSH session
-eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
-
-sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
-service ssh start
 sed -i "s/{PORT}/$PORT/g" /etc/apache2/apache2.conf
 mkdir /var/lock/apache2
 mkdir /var/run/apache2
